7f0eea0472f88c20686d276d922b1fce09964b06
heinrichlaszlo <43448196+heinrichlaszlo@users.noreply.github.com>
2023-02-24 18:42:39 +0100

    refactor: refact saveDummyTransportData

--- src/main/java/com/transportdatamanagementsystem/TransportDataManagementSystemApplication.java
+++ src/main/java/com/transportdatamanagementsystem/TransportDataManagementSystemApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class TransportDataManagementSystemApplication implements CommandLineRunner {
 
@@ -22,12 +24,12 @@ public class TransportDataManagementSystemApplication implements CommandLineRunn
 
 	@Override
 	public void run(String... args) throws Exception {
-		saveDummyTransportData("testFreight1", "testFrom1", "testWhere1");
-		saveDummyTransportData("testFreight2", "testFrom2", "testWhere2");
-		saveDummyTransportData("testFreight3", "testFrom3", "testWhere3");
+		saveDummyTransportData("testFreight1","25", "testFrom1", "testWhere1", LocalDate.of(2022,11,11));
+		saveDummyTransportData("testFreight2","30", "testFrom2", "testWhere2", LocalDate.of(2021,11,10));
+		saveDummyTransportData("testFreight3","24", "testFrom3", "testWhere3",LocalDate.of(2012,07,11));
 	}
 
-	private void saveDummyTransportData(String freight, String from, String where){
-		transportDataRepository.save(TransportData.builder().freight(freight).fromWhereDeliveryPlace(from).whereDeliveryPlace(where).build());
+	private void saveDummyTransportData(String freight, String weight , String from, String where, LocalDate date){
+		transportDataRepository.save(TransportData.builder().freight(freight).fromWhereDeliveryPlace(from).whereDeliveryPlace(where).weight(weight).date(date).build());
 	}
 }
