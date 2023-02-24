b89329fc5b3f17563507217d6e63612a05acdd8a
heinrichlaszlo <43448196+heinrichlaszlo@users.noreply.github.com>
2023-02-24 18:20:49 +0100

    initial commit

---
+++ src/test/java/com/transportdatamanagementsystem/TransportDataManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportdatamanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportDataManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
