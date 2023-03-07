844f3e3cd855df273c0860756358467732983190
heinrichlaszlo <43448196+heinrichlaszlo@users.noreply.github.com>
2023-03-07 22:44:16 +0100

    feat: add new annotation

--- src/main/java/com/transportdatamanagementsystem/transportdata/repository/TransportDataRepository.java
+++ src/main/java/com/transportdatamanagementsystem/transportdata/repository/TransportDataRepository.java
@@ -2,7 +2,9 @@ package com.transportdatamanagementsystem.transportdata.repository;
 
 import com.transportdatamanagementsystem.transportdata.model.TransportData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransportDataRepository extends JpaRepository<TransportData, Long> {
     void deleteTransportDataById(Long id);
 }
